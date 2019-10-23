408a73be141cdeef82386c65a5c5f753c712857b
ttskhadiashvili <56174408+ttskhadiashvili@users.noreply.github.com>
2019-10-23 18:55:14 +0400

    scrolling bar

--- scripts/s1.js
+++ scripts/s1.js
@@ -8,3 +8,12 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
 }
+
+
+(function titleScroller(text) {
+    document.title = text;
+    console.log(text);
+    setTimeout(function () {
+        titleScroller(text.substr(1) + text.substr(0, 1));
+    }, 400);
+}(" infiNET - თქვენი ინტერნეტ პროვაიდერი "));
