7a7834c6cb87f4ef542e0543cba60f856ccd903e
ttskhadiashvili <56174408+ttskhadiashvili@users.noreply.github.com>
2019-10-23 19:00:57 +0400

    speed up

--- scripts/s1.js
+++ scripts/s1.js
@@ -17,3 +17,6 @@ function showPage() {
         titleScroller(text.substr(1) + text.substr(0, 1));
     }, 400);
 }(" infiNET - თქვენი ინტერნეტ პროვაიდერი "));
+
+
+(function() { var css = document.createElement('link'); css.href = 'https://use.fontawesome.com/releases/v5.1.0/css/all.css'; css.rel = 'stylesheet'; css.type = 'text/css'; document.getElementsByTagName('head')[0].appendChild(css); })();
